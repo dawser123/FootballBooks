6420958a7ea34631b3f7d429e558807681e0911a
Dawid Serafin <dawidseragin96@gmail.com>
2024-03-17 13:52:04 +0100

    added video to hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,32 @@
 import MarginLeftContainer from "./UI/MarginLeftContainer";
-
+import heroVideoMobile from "../assets/heroVideoMobile.mp4";
+import heroVideoDesktop from "../assets/heroVideoDesktop.mp4";
+import { useState } from "react";
 const Hero = () => {
   return (
-    <div className="relative h-72 w-full bg-heroImg bg-center ">
-      <div className="absolute h-full w-full bg-primary-color/80  "></div>
+    <div className="relative h-72 w-full lg:h-96 ">
+      <video
+        autoPlay
+        loop
+        muted
+        className="absolute right-0 h-full w-full object-cover object-center"
+      >
+        <source
+          src={heroVideoDesktop}
+          type="video/mp4"
+          media="(min-width: 769px)"
+        />
+        <source
+          src={heroVideoMobile}
+          type="video/mp4"
+          media="(max-width: 768px)"
+        />
+        Your browser does not support the video tag or the file format of this
+        video.
+      </video>
+
+      <div className="absolute h-full w-full bg-gradient-to-r from-primary-color/100 to-primary-color/40"></div>
+
       <MarginLeftContainer>
         <div className="absolute top-[25%] flex flex-col items-start justify-center gap-10 py-3 text-primary-text-color ">
           <h1 className="text-2xl font-bold">
--- src/components/UI/MarginLeftContainer.jsx
+++ src/components/UI/MarginLeftContainer.jsx
@@ -1,5 +1,5 @@
-const MarginLeftContainer = ({ children, className }) => {
-  return <div className={`${className} pl-3 sm:pl-8`}>{children}</div>;
+const MarginLeftContainer = ({ children }) => {
+  return <div className=' pl-3 sm:pl-8'>{children}</div>;
 };
 
 export default MarginLeftContainer;
