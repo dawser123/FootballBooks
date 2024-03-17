5f15c8ee81ca8b4440afb9b5bf3d3ab375d82838
Dawid Serafin <dawidseragin96@gmail.com>
2024-03-17 14:03:55 +0100

    loader spinner added

--- src/components/DataLoadingMessage.jsx
+++ src/components/DataLoadingMessage.jsx
@@ -1,8 +1,9 @@
 const DataLoadingMessage = () => {
   return (
-    <p className="text-bold text-center  text-xl text-primary-text-color">
-      Loading data...
-    </p>
+    <div
+    className="  p-6 text-primary-text-color inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+    role="status">
+   </div>
   );
 };
 export default DataLoadingMessage;
