61ac5f37aa41d24e10b2f08178d762634b9746d0
Dawid Serafin <dawidseragin96@gmail.com>
2024-05-21 18:39:40 +0200

    about us view added

--- src/views/Aboutus.jsx
+++ src/views/Aboutus.jsx
@@ -1,8 +1,25 @@
-
 const Aboutus = () => {
   return (
-    <div>Aboutus</div>
-  )
-}
+    <div className="px-3 sm:px-8">
+      <h2 className="mb-5 py-10 text-center text-2xl text-primary-text-color">
+        About FootballBooks
+      </h2>
+      <div className="mx-auto flex flex-col items-center justify-center gap-10 pb-28 text-primary-text-color md:px-8 lg:max-w-[80%] ">
+        <div>
+          <h3 className="font-bold my-2">Explore exclusive football content</h3>
+          <p>
+          We are passionate football enthusiasts who have built this platform to provide fans of football books access to the best titles related to their favorite sport.
+          </p>
+        </div>
+        <div>
+          <h3 className="font-bold my-2">Get involved with FootballBooks</h3>
+          <p>
+          Join our community and enjoy access to exclusive content, interviews with football stars, and unique stories related to the sport.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Aboutus
+export default Aboutus;
