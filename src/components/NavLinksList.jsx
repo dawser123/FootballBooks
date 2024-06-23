b05f280b956f0914ee4dfd5e9104c6cdeb58172d
Dawid Serafin <dawidseragin96@gmail.com>
2024-06-23 14:20:01 +0200

    login navigation link update

--- src/components/NavLinksList.jsx
+++ src/components/NavLinksList.jsx
@@ -1,12 +1,11 @@
 import NavItem from "./NavItem";
-import { NavLink } from "react-router-dom";
 const NavLinksList = () => {
   return (
     <>
       <NavItem to={"/"}>Home</NavItem>
-      <NavLink to={"/search"} className="cursor-pointer text-2xl">
+      <NavItem to={"/search"} className="cursor-pointer text-2xl">
         Search
-      </NavLink>
+      </NavItem>
       <NavItem to="/my-list">My list</NavItem>
       <NavItem to={"/contact"}>Contact</NavItem>
     </>
