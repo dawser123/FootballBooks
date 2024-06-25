967b41b057539b2a3eb04a5e2d6577d98709b160
Dawid Serafin <dawidseragin96@gmail.com>
2024-06-25 20:01:34 +0200

    update navlink component

--- src/components/NavLinksList.jsx
+++ src/components/NavLinksList.jsx
@@ -3,10 +3,8 @@ const NavLinksList = () => {
   return (
     <>
       <NavItem to={"/"}>Home</NavItem>
-      <NavItem to={"/search"} className="cursor-pointer text-2xl">
-        Search
-      </NavItem>
-      <NavItem to="/my-list">My list</NavItem>
+      <NavItem to={"/search"}>Search</NavItem>
+      <NavItem to={"/my-list"}>My list</NavItem>
       <NavItem to={"/contact"}>Contact</NavItem>
     </>
   );
