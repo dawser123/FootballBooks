c6f6049a482d7329129b7d79104c46294e72c8b8
Dawid <dawidserafin96@gmail.com>
2025-03-29 12:40:00 +0100

    update API requestNewest

--- src/api/requests.js
+++ src/api/requests.js
@@ -1,8 +1,9 @@
 const key = import.meta.env.VITE_REACT_APP_GOOGLE_BOOKS_API_KEY;
+const actualYear = new Date().getFullYear();
 const requests = {
   requestPopular: `https://www.googleapis.com/books/v1/volumes?q=football+clubs&orderBy=relevance&maxResults=20&key=${key} `,
-  requestNewest: `https://www.googleapis.com/books/v1/volumes?q=football+clubs&orderBy=newest&maxResults=20&key=${key}`,
+  requestNewest: `https://www.googleapis.com/books/v1/volumes?q=football+clubs&maxResults=20&filter=ebooks&printType=books&orderBy=newest&publishedDate=${actualYear}&key=${key}`,
   requestPopularByLeague: `https://www.googleapis.com/books/v1/volumes?q={league}+football+clubs&orderBy=relevance&maxResults=10&key=${key}`,
-  requestSearch:`https://www.googleapis.com/books/v1/volumes?q=intitle:football+club&orderBy=relevance&maxResults=20&key=${key}`
+  requestSearch: `https://www.googleapis.com/books/v1/volumes?q=intitle:football+club&orderBy=relevance&maxResults=20&key=${key}`,
 };
 export default requests;
