86b3dbaccfd61146a16ac9d530f7c45c6942620f
Dawid <dawidserafin96@gmail.com>
2025-03-29 12:42:05 +0100

    contact test update

--- tests/contactPageTest.spec.js
+++ tests/contactPageTest.spec.js
@@ -1,8 +1,42 @@
 import { test, expect } from '@playwright/test';
 
-test('Should show contact page', async ({ page }) => {
-  await page.goto('http://localhost:5173/');
+test.describe('user navigate to contact page',  () => {
+test('successful navigate to Contact page ', async ({page}) => {
+  //Arrange 
+  await page.goto('/');
+  //Act
   await page.getByRole('link', { name: 'Contact' }).click();
-  await expect(page.locator('#root')).toContainText('Contact us');
-  await expect(page.locator('div').filter({ hasText: 'Submit' }).nth(4)).toBeVisible();
+  //Assert
+  await expect(page.getByRole('heading', { name: 'Contact us' })).toBeVisible();
+ })
+test('successful message sent  ', async ({page}) => {
+  //Arrange
+  await page.goto('/');
+  const loginUserName = "test@gmail.com";
+  const userMessage = "12345678910";
+  //Act
+  await page.getByRole('link', { name: 'Contact' }).click();
+  await page.getByPlaceholder('Name').fill(loginUserName);
+  await page.getByPlaceholder('Your message').fill(userMessage);
+  await page.getByRole('button', { name: 'Submit' }).click();
+  //Assert
+  await expect(page.getByText('Your message was sent successfully !')).toBeVisible();
+
+ })
+test('unsuccessful message sent  ', async ({page}) => {
+   //Arrange
+   await page.goto('/');
+   const loginUserName = "test@gmail.com";
+   const userMessage = "12345";
+   //Act
+   await page.getByRole('link', { name: 'Contact' }).click();
+   await page.getByPlaceholder('Name').fill(loginUserName);
+   await page.getByPlaceholder('Your message').fill(userMessage);
+   await page.getByRole('button', { name: 'Submit' }).click();
+   //Assert
+   await expect(page.getByText('Your message must contain at least 10 characters')).toBeVisible();
+ })
+
+
+ 
 });
