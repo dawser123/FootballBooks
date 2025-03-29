8e1ec4612e92035f3a978b8fa6d8f5ba245f6343
Dawid <dawidserafin96@gmail.com>
2025-03-29 12:42:30 +0100

    newsletter test update

--- tests/newsletterSubscriptionTest.spec.js
+++ tests/newsletterSubscriptionTest.spec.js
@@ -1,8 +1,32 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('Should show newsletter', async ({ page }) => {
-  await page.goto('http://localhost:5173/');
-  await page.getByRole('link', { name: 'Logo of Football Books' }).click();
-  await page.getByLabel('Open Newsletter').click();
-  await expect(page.getByText('Subscribe to newsletterSubscribe')).toBeVisible();
-})
+test.describe("Should show newsletter", () => {
+  test("should open the newsletter modal", async ({ page }) => {
+    //Arrange
+    await page.goto("/");
+    //Act
+    await page.getByLabel("Open Newsletter").click();
+    //Assert
+    await expect(page.getByText("Subscribe to newsletter", { exact: true }),).toBeVisible();
+  });
+  test("should subscribe successfully", async ({ page }) => {
+    //Arrange
+    await page.goto("/");
+    //Act
+    await page.getByLabel("Open Newsletter").click();
+    await page.getByRole("button", { name: "Subscribe" }).click();
+    //Assert
+    await expect(page.getByText("Subscription successful !")).toBeVisible();
+  });
+  test("should fail to subscribe with invalid email", async ({ page }) => {
+    //Arrange
+    const invalidUserMail = "test@gmail";
+    await page.goto("/");
+    //Act
+    await page.getByLabel("Open Newsletter").click();
+    await page.getByPlaceholder("Email address").fill(invalidUserMail);
+    await page.getByRole("button", { name: "Subscribe" }).click();
+    //Assert
+    await expect(page.getByText("Enter valid email and try")).toBeVisible();
+  });
+});
