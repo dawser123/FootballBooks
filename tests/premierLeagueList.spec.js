1e77c2db7f8a7db7fd0b314ac4619773e005d12d
Dawid <dawidserafin96@gmail.com>
2025-03-29 12:41:51 +0100

    leagueList test update

--- tests/premierLeagueList.spec.js
+++ tests/premierLeagueList.spec.js
@@ -1,8 +1,14 @@
 import { test, expect } from '@playwright/test';
 
-test('Should show premier leage list', async ({ page }) => {
-  await page.goto('http://localhost:5173/');
-  await page.getByRole('link', { name: 'Logo of Football Books' }).click();
-  await page.getByRole('link', { name: 'premierleague logo' }).click();
-  await expect(page.getByText('Premier League')).toBeVisible();
+test.describe('Should show premier league list', () => {
+
+  test("should open the newsletter modal", async ({ page }) => {
+    //Arrange
+    await page.goto("/");
+    //Act
+    await page.getByRole('link', { name: 'Logo of Football Books' }).click();
+    await page.getByRole('link', { name: 'premierleague logo' }).click();
+    //Assert
+    await expect(page.getByRole('heading', { name: 'Top 10 books inPremier League' })).toBeVisible();
+  });
 });
